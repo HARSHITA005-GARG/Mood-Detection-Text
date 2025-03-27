10fc468e2d955d62b691d175ebe4bf3e47799e4b
Harshita <126266272+HARSHITA005-GARG@users.noreply.github.com>
2025-03-28 01:37:18 +0530

    Update railway_install.sh

--- railway_install.sh
+++ railway_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update && apt-get install -y portaudio19-dev
-python3 -m venv /opt/venv
-source /opt/venv/bin/activate
+python3 -m venv myenv
+source myenv/Scripts/activate
 pip install -r requirements.txt
